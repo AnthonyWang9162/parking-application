d398738fa6153e445175af6d0319849d2efefbb6
AnthonyWang9162 <anthony9162@gmail.com>
2024-07-08 13:38:44 +0800

    Update parking-application.py

--- parking-application.py
+++ parking-application.py
@@ -23,7 +23,6 @@ def connect_db():
     conn = sqlite3.connect(local_db_path)
     return conn
     
-@st.cache_data
 def download_db(file_id, destination):
     request = service.files().get_media(fileId=file_id)
     fh = io.FileIO(destination, 'wb')
