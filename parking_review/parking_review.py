88854318f6d1541c7001cd6dc890fb31911e0544
AnthonyWang9162 <anthony9162@gmail.com>
2024-07-26 11:08:41 +0800

    Update parking_review.py

--- parking_review/parking_review.py
+++ parking_review/parking_review.py
@@ -169,11 +169,11 @@ def load_data6(current):
         A.車牌號碼,
         A.聯絡電話,
         A.身分註記,
-        B.車位編號,
+        A.車位編號,
         B.車位備註,
         B.使用狀態
-    FROM  停車位 B
-    LEFT JOIN 免申請 A ON A.車位編號 = B.車位編號
+    FROM 免申請 A
+    LEFT JOIN 停車位 B ON A.車位編號 = B.車位編號
     UNION
     SELECT 
         C.姓名代號,
@@ -189,7 +189,6 @@ def load_data6(current):
     INNER JOIN 繳費紀錄 D ON C.期別 = D.期別 AND C.姓名代號 = D.姓名代號
     LEFT JOIN 停車位 B ON D.車位編號 = B.車位編號
     WHERE C.期別 = ?
-    AND B.車位編號 NOT IN (SELECT 車位編號 FROM 繳費紀錄)
     """
     df = pd.read_sql_query(query, conn, params=(current,))
     conn.close()
