b3b2353caa9ec1493046ac51bf61a81445d50af3
AnthonyWang9162 <anthony9162@gmail.com>
2024-10-22 09:48:40 +0800

    Update parking_review.py

--- parking_review/parking_review.py
+++ parking_review/parking_review.py
@@ -10,6 +10,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import os
+import time
 from reportlab.lib.pagesizes import letter
 from reportlab.lib import colors
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
